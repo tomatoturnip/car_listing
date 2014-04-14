45c3aecbe2f254bece9456427ffd54980d96a4bf
Lydia Ho <lyd.w.ho@gmail.com>
2014-04-14 10:33:54 -0400

    created car feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140414140150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: true do |t|
+    t.string   "color",       null: false
+    t.string   "year",        null: false
+    t.string   "mileage",     null: false
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
---
+++ spec/features/recording_a_car_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'record a car', %Q{
+  As a car salesperson
+  I want to record a newly acquired car
+  So that I can list it in my lot
+  } do
+
+# Acceptance Criteria:
+
+# * I must specify the color, year, and mileage of the car.
+# * Only years from 1980 and above can be specified.
+# * I can optionally specify a description of the car.
+# * If I enter all of the required information in the required formats, the car is recorded.
+# * If I do not specify all of the required information in the required formats, the car is not recorded and I am presented with errors.
+# * Upon successfully creating a car, I am redirected so that I can create another car.
+
+  scenario 'creating a car' do
+    visit new_car_path
+    expect(page).to have_content('Create a new car')
+    fill_in 'Color', with: 'Black'
+    select '2006', from: 'Year'
+    fill_in 'Mileage', with: '100000'
+    fill_in 'Description', with: 'A rather ugly car'
+
+
+    click_button 'Create Car'
+    expect(page).to have_content("Successfully created car")
+    expect(current_path).to eql(new_car_path)
+  end
+end
